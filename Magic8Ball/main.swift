3007353fd0c4989b44d3ecb8a8bedf9cec8c678b
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-09-16 09:57:20 -0400

    Added a user interface for the program. Now the user can input questions and the program will set a value to return to the user as an answer.
    Now I will use a switch statement to add the answers

--- Magic8Ball/main.swift
+++ Magic8Ball/main.swift
@@ -7,7 +7,19 @@
 
 import Foundation
 
+// Program name
 print("Magic 8-Ball 🎱")
-print("")
+print("===============")
+
+// Ask for the user's question
+print("Ask a question")
+// Get input as a non-optional String
+let input = readLine()!
+
+// Select a random value
+let value = Int.random(in: 1...20)
+
+// Print random value
+print("\(value)")
 
 
